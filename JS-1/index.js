a85305fe88281338d90a61b09f8cf281bee81762
DenisProxx <Denisproxx@gmail.com>
2021-12-17 15:04:30 +0400

    master

---
+++ JS-1/index.js
@@ -0,0 +1,49 @@
+/* Задание 1*/
+// let myName = prompt("Введите имя" , "");
+// alert("Привет " + myName + "!");
+
+/* Зазание 2*/
+// let years = prompt("Введите год рождения" , 0);
+// const year =("2021");
+// alert(year - years);
+
+/* Зазание 3*/
+// let length = prompt("Введите длину квадрата");
+// alert(length * 4);
+
+/* Зазание 4*/
+// let radius = prompt("Введите радиус окружности");
+// const pi =("3.1415926");
+// const result = Math.pow(radius, 2);
+// alert(pi * result);
+
+/* Зазание 5*/
+// let dist = prompt("Введите расстояние" , 0 );
+// let time = prompt("Введите время" , 0);
+// alert(dist/time + "км/ч");
+
+/* Зазание 6*/
+// let dollar = prompt("Введите сумму в долларах" , 0);
+// const kurs =("0.85");
+// alert(dollar*kurs +"евро");
+
+/* Зазание 7*/
+// let vol =prompt("Укажите объем флешки", 0);
+// let number =((vol*1024)/820);
+// alert(number);
+
+/* Зазание 8*/
+// let sum =prompt("Введите сумму денег" , 0);
+// let price =prompt("Введите цену шоколадки" , 0);
+// let number =(sum/price);
+// let ost =(sum-price);
+// alert(number);
+// alert(sum-number);
+
+/* Зазание 9*/
+// let rez=prompt("Введите число");
+// alert(rez.split("").reverse().join(""));
+
+/* Зазание 10*/
+// const number = prompt("Введите число");
+// number % 2 == 0 ? alert("четное число") : alert("нечетное число");
